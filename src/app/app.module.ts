55558ebaedb5e28b037b4dc80b3f8e41525e6072
mxaxel <99214301+mxaxel@users.noreply.github.com>
2022-09-07 15:06:17 -0500

    Revert "create branch skills Closes #13"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
-import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WorkExperienceComponent,
-    EducationComponent
+    WorkExperienceComponent
   ],
   imports: [
     BrowserModule
